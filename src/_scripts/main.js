d51d928138de4483fa57768dbc23c951fe99f0dc
Aaron Ward <aaronward@google.com>
2015-09-20 01:43:11 -0700

    scaffold

---
+++ src/_scripts/main.js
@@ -0,0 +1,15 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+import $ from 'jquery';
+import Link from '../_modules/link/link';
+
+$(() => {
+  new Link(); // Activate Link modules logic
+  console.log('nosegay');
+  setInterval(() => {
+  	document.getElementById('time').innerHTML = Date.now();
+  }, 1);
+});
